cafa0aa730077cc923493781f27538adee25ca16
gavrish.me <gavrish.me@yandex.ru>
2021-01-22 15:53:36 +0300

    свёрстан блок фото без адаптива

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,3 +12,29 @@ const navList = document.querySelector('#nav-list');
 		navList.classList.toggle('header-nav-show');
 		document.querySelector('body').classList.toggle('overflowy')
 	}
+
+
+// Инициализация слайдера фото
+$('.foto-slider-list').slick({
+			dots: true,
+			infinite: false,
+			arrows: false,
+			speed: 300,
+			slidesToShow: 1,
+			slidesToScroll: 1,
+		});
+
+const dotsTrue = document.querySelectorAll('.slick-dots li')
+const dotsFalse = document.querySelectorAll('.foto-tab-item')
+let activeSlide = 0
+let event = new Event("click")
+
+for (let i = 0; i < dotsTrue.length; i++) {
+	dotsFalse[i].addEventListener('click', () => {
+		dotsFalse[activeSlide].classList.toggle('tab-active')
+		dotsFalse[i].classList.toggle('tab-active')
+		activeSlide = i
+		dotsTrue[i].dispatchEvent(event)
+
+	})
+}
