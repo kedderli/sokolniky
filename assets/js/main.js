9927017dbdae3d7e17422717eada839bd5ad5312
gavrish.me <gavrish.me@yandex.ru>
2021-01-22 19:42:24 +0300

    Свёрстан блок фото с адаптивом

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,10 +22,62 @@ $('.foto-slider-list').slick({
 			speed: 300,
 			slidesToShow: 1,
 			slidesToScroll: 1,
+			draggable: false,
+			swipe: false
+		});
+
+const fotoSlider = $('.foto-tab-list').slick({
+			dots: false,
+			infinite: false,
+			speed: 300,
+			slidesToShow: 11,
+			slidesToScroll: 11,
+			prevArrow: '<button type = "button" class = "slick-prev-a"><img src="assets/image/icons/prev.png"></button>',
+	 		nextArrow: '<button type = "button" class = "slick-next-a"><img src="assets/image/icons/next.png"></button>',
+	 		arrows: true,
+			responsive: [
+				{
+					breakpoint: 1200,
+					settings: {
+						slidesToShow: 7,
+						slidesToScroll: 7,
+					}
+				},
+				{
+					breakpoint: 992,
+					settings: {
+						slidesToShow: 6,
+						slidesToScroll: 6,
+					}
+				},
+				{
+					breakpoint: 768,
+					settings: {
+						slidesToShow: 4,
+						slidesToScroll: 4,
+					}
+				},
+				{
+					breakpoint: 576,
+					settings: {
+						slidesToShow: 2,
+						slidesToScroll: 2,
+					}
+				},
+				{
+					breakpoint: 390,
+					settings: {
+						slidesToShow: 1,
+						slidesToScroll: 1,
+					}
+				}
+
+			]
 		});
 
 const dotsTrue = document.querySelectorAll('.slick-dots li')
 const dotsFalse = document.querySelectorAll('.foto-tab-item')
+
 let activeSlide = 0
 let event = new Event("click")
 
@@ -35,6 +87,41 @@ for (let i = 0; i < dotsTrue.length; i++) {
 		dotsFalse[i].classList.toggle('tab-active')
 		activeSlide = i
 		dotsTrue[i].dispatchEvent(event)
-
 	})
 }
+
+fotoSlider.on('afterChange', () => {
+	if (document.documentElement.clientWidth <= 390) {
+		let currentSlide = fotoSlider.slick('slickCurrentSlide')
+		dotsFalse[activeSlide].classList.toggle('tab-active')
+		dotsFalse[currentSlide].classList.toggle('tab-active')
+		activeSlide = currentSlide
+		dotsTrue[currentSlide].dispatchEvent(event)
+	}
+})
+
+
+// document.querySelector('.slick-next-a').addEventListener('click', () => {
+// 	console.log(document.documentElement.clientWidth)
+// 	if (document.documentElement.clientWidth <= 390) {
+// 		let currentSlide = fotoSlider.slick('slickCurrentSlide')
+// 		console.log(currentSlide)
+// 		dotsFalse[activeSlide].classList.toggle('tab-active')
+// 		dotsFalse[currentSlide].classList.toggle('tab-active')
+// 		activeSlide = currentSlide
+// 		dotsTrue[currentSlide].dispatchEvent(event)
+// 	}
+// })
+
+// document.querySelector('.slick-prev-a').addEventListener('click', () => {
+// 	if (document.documentElement.clientWidth <= 390) {
+// 		let currentSlide = fotoSlider.slick('slickCurrentSlide')
+// 		console.log(currentSlide)
+// 		dotsFalse[activeSlide].classList.toggle('tab-active')
+// 		dotsFalse[currentSlide].classList.toggle('tab-active')
+// 		activeSlide = currentSlide
+// 		dotsTrue[currentSlide].dispatchEvent(event)
+// 	}
+// })
+
+
